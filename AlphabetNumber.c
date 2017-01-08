9f5fe61bb90b10fe12b0e05954d62f44c5357abe
Kaid02 <kabirS317@gmail.com>
2017-01-08 13:04:43 +0545

    Add files via upload

---
+++ AlphabetNumber.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+	int main()
+	{
+		
+		int a=65,b=97;
+		
+	    while(("a>=65&&a<=90)||(b=>97&&b<=122"))
+	    {
+		printf("%c %c",a,b);
+	    a++;
+	    b++;
+	    }
+			
+		return 0;
+		
+	}
